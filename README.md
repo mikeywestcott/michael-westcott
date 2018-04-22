256c88c034ad80a2ba5537a8c69a5141241ac903
Kieron Scott <kieronscott@kierons-mbp.home>
2018-04-22 19:09:48 +0100

    revert

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# my-site
+# michael-westcott
--- static/scripts/scripts.js
+++ static/scripts/scripts.js
@@ -1,1 +1,30 @@
 document.documentElement.className = "js";
+
+$( "#nav-toggle" ).click(function() {
+  $( "body" ).toggleClass( "nav-open" );
+});
+
+if($('.fluid-video').length >0 ){
+    $(".fluid-video").fitVids({
+      customSelector: 'iframe[src*="facebook"]'
+  });
+}
+
+if($('.quote-rotator').length >0 ){
+  var slideIndex = 0;
+  carousel();
+
+  function carousel() {
+    var i;
+    var x = document.getElementsByClassName("quote");
+    for (i = 0; i < x.length; i++) {
+      x[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > x.length) {
+      slideIndex = 1
+    }
+    x[slideIndex - 1].style.display = "block";
+    setTimeout(carousel, 20000);
+  }
+}
